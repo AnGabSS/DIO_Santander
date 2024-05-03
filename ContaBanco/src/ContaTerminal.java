3224af5f539f8fc17ac9f4f3fe3d0ed533d07665
AnGabSS <angelo.gs.santana@outlook.com>
2024-05-02 22:20:20 -0300

    Challenge completed

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) {
+
+        int number;
+        String agency, clientName;
+        BigDecimal balance;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da sua Agência: ");
+        number = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Por favor, digite o nome da sua Agência: ");
+        agency = sc.nextLine();
+        System.out.println("Por favor, digite o seu nome: ");
+        clientName = sc.nextLine();
+        System.out.println("Por favor, digite o seu saldo: ");
+        balance = sc.nextBigDecimal();
+        sc.close();
+
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agency + ", conta " + number + " e seu saldo " + balance + " já está disponível para saque");
+    }
 }
